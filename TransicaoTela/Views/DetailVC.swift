9f4d9cbb6a49155e9103297d826716195f65e93f
draffa <draffa_ti@hotmail.com>
2020-09-25 22:38:09 -0300

    Incluido nova tela com transicao pelo botao

---
+++ TransicaoTela/Views/DetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  SecondVC.swift
+//  TransicaoTela
+//
+//  Created by Rafael Jose on 25/09/20.
+//
+// Para criar uma view clicar na pasta que vc quer adicionar e selecione o tipo 'Cocoa Touch Class'
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- TransicaoTela/Views/FirstVC.swift
+++ TransicaoTela/Views/FirstVC.swift
@@ -30,6 +30,23 @@ class FirstVC: UIViewController {
         print("viewWillAppear")
         // exibe toda vez em que a tela aparece
     }
-
+    
+    
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        
+        // Preapara a proxima tela
+        // self.performSegue(withIdentifier: "DetailVC", sender: nil)
+        self.performSegue(withIdentifier: "DetailVC", sender: "Rafael")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //
+        let vc: DetailVC? = segue.destination as? DetailVC
+        
+        vc?.view.backgroundColor = .red
+        // vc?.myLabel.text = "Rafael"
+        vc?.myLabel.text = sender as? String
+    }
+    
 }
 
